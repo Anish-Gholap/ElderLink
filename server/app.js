de34ba87b05459ad2c578f2e854e542b427a55ed
Anish-Gholap <anishgholap@gmail.com>
2025-03-18 02:11:05 +0800

    server can run now

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+
+// MongoDB Connection
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
+
+// Middlewares
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+module.exports = app
