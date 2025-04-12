25ea111925a308ebf7bfe3ed6d4e830ca906a46f
Wong Yi Jie <149584960+Yi-992@users.noreply.github.com>
2025-04-12 21:48:21 +0800

    "JSDoc for Model"

--- server/models/event.js
+++ server/models/event.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose')
 
+/**
+ * Mongoose schema for the Event model.
+ * Represents an event created by a user, including details such as title, date, location, description, and attendees.
+ */
 const eventSchema = new mongoose.Schema({
+    /**
+     * The title of the event.
+     * @type {string}
+     * @required
+     * @validate {function} Ensures the title contains only letters, numbers, and spaces.
+     */
     title: {
         type: String,
         required: true,
@@ -11,6 +21,12 @@ const eventSchema = new mongoose.Schema({
             message: 'Title can only contain letters, numbers, and spaces'
         }
     },
+    /**
+     * The date and time of the event.
+     * @type {Date}
+     * @required
+     * @validate {function} Ensures the date is valid and falls within CC operating hours (10am-10pm).
+     */
     date: {
         type: Date,
         required: true,
@@ -27,11 +43,25 @@ const eventSchema = new mongoose.Schema({
             message: 'Date must be valid and during CC operating hours (10am-10pm)'
         }
     },
+    /**
+     * The location of the event.
+     * @type {string}
+     */
     location: String,
+    /**
+     * A description of the event.
+     * @type {string}
+     * @minimumLength 50 - Ensures the description is at least 50 characters long.
+     */
     description: {
         type: String,
         minLength: [50, "Description must be at least 50 characters long"]
     },
+    /**
+     * The number of attendees for the event.
+     * @type {number}
+     * @validate {function} Ensures the number of attendees is positive and does not exceed 10.
+     */
     numAttendees: {
         type: Number,
         validate: [
@@ -49,16 +79,29 @@ const eventSchema = new mongoose.Schema({
             }
         ]
     },
+    /**
+     * The user who created the event.
+     * @type {mongoose.Schema.Types.ObjectId}
+     * @ref 'User'
+     */
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    /**
+     * The list of users attending the event.
+     * @type {Array<mongoose.Schema.Types.ObjectId>}
+     * @ref 'User'
+     */
     attendees: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     }]
 })
-
+/**
+ * Transform the JSON representation of the event.
+ * Converts `_id` to `id` and removes `__v` from the returned object.
+ */
 eventSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
@@ -66,7 +109,9 @@ eventSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
-
+/**
+ * Mongoose model for the Event schema.
+ */
 const Event = mongoose.model('Event', eventSchema)
 
 module.exports = Event
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose')
 
+/**
+ * Mongoose schema for the User model.
+ * Represents a user in the system, including details such as username, name, password hash, phone number, events, and notifications.
+ */
 const userSchema = new mongoose.Schema({
+    /**
+     * The username of the user.
+     * @type {string}
+     * @required
+     * @minLength 3 - Must be at least 3 characters long.
+     * @maxLength 12 - Cannot exceed 12 characters.
+     * @unique
+     * @validate {function} Ensures the username contains only letters and numbers.
+     */
     username: {
         type: String,
         required: true,
@@ -14,6 +27,13 @@ const userSchema = new mongoose.Schema({
             message: 'Username can only contain letters and numbers, no special characters'
         }
     },
+    /**
+     * The full name of the user.
+     * @type {string}
+     * @required
+     * @minLength 3 - Must be at least 3 characters long.
+     * @validate {function} Ensures the name contains only alphabets and spaces.
+     */
     name: {
         type: String,
         required: true,
@@ -25,11 +45,27 @@ const userSchema = new mongoose.Schema({
             message: 'Name can only contain alphabets'
         }
     },
+    /**
+     * The hashed password of the user.
+     * @type {string}
+     */
     passwordHash: String,
+    /**
+     * The list of events created by the user.
+     * @type {Array<mongoose.Schema.Types.ObjectId>}
+     * @ref 'Event'
+     */
     eventsCreated: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Event'
     }],
+    /**
+     * The phone number of the user.
+     * @type {string}
+     * @required
+     * @unique
+     * @validate {function} Ensures the phone number is exactly 8 digits (Singapore format).
+     */
     phoneNumber: {
         type: String,
         required: true,
@@ -43,17 +79,30 @@ const userSchema = new mongoose.Schema({
             }
         ]
     },
+    /**
+     * The list of events the user is attending.
+     * @type {Array<mongoose.Schema.Types.ObjectId>}
+     * @ref 'Event'
+     */
     eventsAttending: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Event'
     }],
+    /**
+     * The list of notifications associated with the user.
+     * @type {Array<mongoose.Schema.Types.ObjectId>}
+     * @ref 'Notification'
+     */
     notifications: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Notification'
     }]
 })
 
-
+/**
+ * Transform the JSON representation of the user.
+ * Converts `_id` to `id` and removes sensitive fields like `passwordHash` and `__v`.
+ */
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
@@ -62,7 +111,9 @@ userSchema.set('toJSON', {
         delete returnedObject.passwordHash
     }
 })
-
+/**
+ * Mongoose model for the User schema.
+ */
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
