288309b11fae120266997e0847cabd62d4632ced
Wong Yi Jie <149584960+Yi-992@users.noreply.github.com>
2025-04-12 21:29:56 +0800

    "JSDoc for Login"

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -2,7 +2,15 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
-//login user
+/**
+ * Log in a user by verifying their credentials and generating a JWT token.
+ * @param {Object} request - Express request object.
+ * @param {Object} request.body - The request body containing username and password.
+ * @param {string} request.body.username - The username of the user.
+ * @param {string} request.body.password - The password of the user.
+ * @param {Object} response - Express response object.
+ * @returns {Promise<void>} Responds with a JWT token and user details if login is successful, or an error message if login fails.
+ */
 const loginUser = async (request, response) => {
   const {username, password} = request.body
 
